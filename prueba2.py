518502c4df9bc9a411311221a20fda63b48fbacd
Jasonjeik <jasonb@uninorte.edu.co>
2025-03-08 07:54:48 -0500

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -183,6 +183,13 @@ def pagina_modelo():
     st.write("Se ha calculado un **Mean Absolute Percentage Error (MAPE)** de **11.64%**, lo que indica un buen desempeño en la estimación de costos.")
     st.write("El modelo ha sido entrenado con datos reales de transporte y utiliza variables como el tipo de vehículo, la distancia y el peso para hacer las predicciones.")
 
+import streamlit as st
+from PIL import Image
+
+# Leer y mostrar la imagen clusters.png
+image = Image.open("clusters.png")
+st.image(image, caption="Mapa de Clusters", use_column_width=True)
+
 df = load_data()
 
 @st.cache_data
