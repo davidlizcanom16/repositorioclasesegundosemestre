e7e2b700dafa7a7cf7b3032d78d7486b80c29d9f
Jasonjeik <jasonb@uninorte.edu.co>
2025-03-08 09:18:34 -0500

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -77,6 +77,7 @@ def pagina_introduccion():
                 fill=True,
             ).add_to(m)
         st_folium(m, width=1000, height=600)
+        folium_static(m)
     else:
         st.warning("No hay datos disponibles para mostrar en el mapa.")
 
